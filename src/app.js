88e103c2acb23882edbd7480d6c06c05988dde01
Junya Hosokawa <hosokawa@morijyobi.com>
2016-07-02 14:04:16 +0900

    格子状に　cover_png　を配置する

--- src/app.js
+++ src/app.js
@@ -14,10 +14,19 @@ var gameScene = cc.Scene.extend({
 var game = cc.Layer.extend({
     ctor: function() {
         this._super();
+        /*
         var size = cc.director.getWinSize();
         var tile = cc.Sprite.create(res.cover_png);　
         tile.setPosition(size.width/2,size.height/2);　
         this.addChild(tile);
+        */
+        for(i=0;i<16;i++){
+          var tile = cc.Sprite.create(res.cover_png);
+          this.addChild(tile,0);
+          //タイルを格子状に配置する計算式
+          tile.setPosition(49+i%4*74,400-Math.floor(i/4)*74);
+        }
+
         return true;
 
     },
